86530539d1adb49cb6074c2b94a7f90ad031390c
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-03-27 07:28:29 -0700

    task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -10,18 +10,19 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *new_tree;
+	binary_tree_t *new_root;
 
 	if (tree == NULL || tree->right == NULL)
-		return (NULL);
+		return (tree);
 
-	new_tree = tree->right;
+	new_root = tree->right;
 
-	tree->right = new_tree->left;
-	tree->parent = new_tree;
+	tree->right = new_root->left;
+	if (new_root->left != NULL)
+		new_root->left->parent = tree;
+	new_root->left = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
 
-	new_tree->left = tree;
-	new_tree->parent = tree->parent;
-
-	return (new_tree);
+	return (new_root);
 }
